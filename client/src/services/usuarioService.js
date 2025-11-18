36f490b437d88e3f71e32c433c47d468d8b36e7a
EnriqueJassoBenito <20213tn033@utez.edu.mx>
2025-11-18 00:46:45 -0600

    [FIX] Error en los puertos

--- client/src/services/usuarioService.js
+++ client/src/services/usuarioService.js
@@ -1,6 +1,6 @@
 const ENV = import.meta.env;
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}/usuarios`;
+const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
 
 export async function obtenerUsuarios() {
     const response = await fetch(`${API_URL}`);
